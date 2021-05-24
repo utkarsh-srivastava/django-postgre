a2f83081af1964d10c143069996f1ec04ae9e609
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-05-25 00:25:00 +0530

    customized the admin view for tables. Added attr, search and filters

--- listings/admin.py
+++ listings/admin.py
@@ -4,4 +4,17 @@ from .models import Listing
 
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtors',)
+    # makes them clickable
+    list_display_links = ('id', 'title')
+    # adds filter
+    list_filter = ('realtors',)
+    # make boolean values clickable and from here we can publish and publish
+    list_editable = ('is_published',)
+    # add searching option
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'city', 'zipcode', 'price')
+    # add pagination
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
