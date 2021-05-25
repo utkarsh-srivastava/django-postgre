f7fa94a7b997e9616f289c8fc699fc58f0ce35fd
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-05-25 23:26:23 +0530

    listings and about page functionality complete

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 # Create your views here.
 
 def index(request):
-    listings = Listing.objects.all()
+    # descending date order and if it is published
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # listings = Listing.objects.all()
+
+    paginator = Paginator(listings, 2)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request, 'listings/listings.html', context)
