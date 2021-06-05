daa3ff2970d79154d5f92c89d0aa7d43e7b6c0e2
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-06-05 14:11:28 +0530

    [Validatio] : for logged in user no duplicate request for same property

--- contacts/views.py
+++ contacts/views.py
@@ -15,6 +15,16 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+
+        # check if the user has already made an inquiry
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry')
+                return redirect('/listings/'+listing_id)
         
         contact = Contact(listing=listing, listing_id=listing_id,name=name,email=email,phone=phone,message=message,user_id=user_id)
 
